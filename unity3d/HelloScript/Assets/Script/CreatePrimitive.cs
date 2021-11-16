b27b2cea9607507222b24fef9e5b3ab851116f67
chenkongliang <chenkongliang@haochengji100.com>
2021-11-16 19:31:40 +0800

    添加unity 	new file:   unity3d/HelloScript/ 	new file:   unity3d/HelloScript2/

---
+++ unity3d/HelloScript/Assets/Script/CreatePrimitive.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatePrimitive : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnGUI()
+    {
+        if (GUILayout.Button("CreateCube", GUILayout.Height(50)))
+        {
+            GameObject m_cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            m_cube.AddComponent<Rigidbody>();
+            m_cube.GetComponent<Renderer>().material.color = Color.blue;
+            m_cube.transform.position = new Vector3(0, 10, 0);
+        }
+        if (GUILayout.Button("CreateSphere", GUILayout.Height(50)))
+        {
+            GameObject m_sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            m_sphere.AddComponent<Rigidbody>();
+            m_sphere.GetComponent<Renderer>().material.color = Color.red;
+            m_sphere.transform.position = new Vector3(0, 10, 0);
+        }
+    }
+}
---
+++ unity3d/HelloScript2/Assets/MyScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyScript : MonoBehaviour {
+    public GameObject myCube;
+
+    private bool isForward = false;
+    public int transSpeed = 100;
+    public float rotaSpeed = 10.5f;
+
+    private bool isScaled = false;
+    public float scale = 3;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnGUI()
+    {
+        if (GUILayout.Button("移动立方体"))
+        {
+            Vector3 vector = isForward ? Vector3.forward : Vector3.back;
+            myCube.transform.Translate(vector * transSpeed * Time.deltaTime, Space.World);
+            isForward = !isForward;
+        }
+
+        if (GUILayout.Button("旋转立方体"))
+        {
+            myCube.transform.Rotate(Vector3.up * rotaSpeed, Space.World);
+        }
+
+        if (GUILayout.Button("缩放立方体"))
+        {
+            scale = isScaled ? 1 : 3;
+            myCube.transform.localScale = new Vector3(scale, scale, scale);
+            isScaled = !isScaled;
+        }
+    }
+}
