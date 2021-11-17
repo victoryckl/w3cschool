d378fd9d414097157264cc1408cc01eb6f544cfb
chenkongliang <chenkongliang@haochengji100.com>
2021-11-17 17:16:55 +0800

    添加Unity 	modified:   HelloUnity2/.gitignore 	new file:   UnitOnGui/ 	new file:   UnityUGUI/

---
+++ unity3d/UnitOnGui/Assets/Scripts/Test.js
@@ -0,0 +1,85 @@
+﻿#pragma strict
+
+function Start () {
+	
+}
+
+function Update () {
+	
+}
+
+var count = 1;
+
+var btnTexture:Texture;
+var atnTexture:Texture;
+var textureToDisplay:Texture2D;
+
+public static var backgroundColor:Color;
+public static var color:Color;
+
+var stringToEdit:String="Hello Edit";
+
+var scrollPosition:Vector2 = Vector2.zero;
+
+var hSliderValue:float = 0.0;
+var vSliderValue:float = 0.0;
+
+var toolbarInt:int = 0;
+var toolbarStrings:String[] = ["toolbar1", "toolbar2", "toolbar3"];
+
+function OnGUI() {
+	if (GUI.Button(Rect(0,0,100,50), "click here")) {
+		print("you have click here! "+count);
+		count++;
+	}
+
+	if (!btnTexture) {
+		Debug.LogError("Please assign a texture on the inspector");
+		return;
+	}
+	if (!atnTexture) {
+		Debug.LogError("Please assign a texture on the inspector");
+		return;
+	}
+
+	if (GUI.Button(Rect(Screen.width/2-50, Screen.height/2+130, 70, 70),atnTexture)){
+		Application.LoadLevel("play");
+	}
+	if (GUI.Button(Rect(Screen.width/2+30, Screen.height/2+130, 70, 70), btnTexture)) {
+		Application.LoadLevel("exit");
+	}
+
+	GUI.Box(Rect(Screen.width-100,0,100,50), "Box\nTop-Right");
+	GUI.Box(Rect(0, Screen.height-50, 100,50), "Box\nButtom-Left");
+	GUI.Box(Rect(Screen.width-100,Screen.height-50, 100,50), "Box\nButtom-Right");
+
+	if (!textureToDisplay) {
+		Debug.LogError("Please assign a texture on the inspector: textureToDisplay");
+		return;
+	}
+
+	GUI.Label(Rect(10, 100, 100, 20),"Hello World!");
+	GUI.Label(Rect(10, 140, textureToDisplay.width, textureToDisplay.height), textureToDisplay);
+
+	GUI.backgroundColor = Color.yellow;
+    GUI.Button(Rect(10, 210, 70, 30), "A button");
+	
+	GUI.color = Color.red;
+	GUI.Box(Rect(200, 50, 50, 50), "A BOX");
+
+	Debug.Log(GUI.TextField(Rect(260,50,100,30), stringToEdit, 25));
+	Debug.Log(GUI.TextArea(Rect(380,50,200,80), "Hello TextArea\n next line"));
+
+	scrollPosition = GUI.BeginScrollView(Rect(580,50,100,100), scrollPosition, Rect(0,0,220,200));
+    GUI.Button(Rect(0, 0, 100, 20), "Top-left");
+    GUI.Button(Rect(120, 0, 100, 20), "Top-right");
+    GUI.Button(Rect(0, 180, 100, 20), "Bottom-left");
+    GUI.Button(Rect(120, 180, 100, 20), "Bottom-right");
+    GUI.EndScrollView();
+
+	hSliderValue = GUI.HorizontalSlider(Rect(100, 150, 100, 10), hSliderValue, 0.0, 10.0);
+	vSliderValue = GUI.VerticalSlider(Rect(130, 160, 30, 60), vSliderValue, 10.0, 0.0);
+
+	toolbarInt = GUI.Toolbar(Rect(25, 320, 250, 30), toolbarInt, toolbarStrings);
+	Debug.Log("toolbarInt="+toolbarInt);
+}
---
+++ unity3d/UnitOnGui/Assets/Scripts/Test2.js
@@ -0,0 +1,52 @@
+﻿#pragma strict
+
+function Start () {
+	
+}
+
+function Update () {
+	
+}
+
+var windowRect:Rect = Rect(300,20,120,50);
+
+var windowRect0:Rect=Rect(500, 20, 120, 50);
+var windowRect1:Rect=Rect(500, 100, 120, 50);
+
+var aTexture:Texture;
+private var toggleTxt:boolean=false;
+private var toggleImg:boolean=false;
+
+function OnGUI() {
+	GUI.Box(Rect(5,35,210,175), GUIContent("Box", "this box has a tooltip"));
+	GUI.Button(Rect(30,85,100,20), "No tooltip here");
+	GUI.Button(Rect(30,120,100,20), 
+		GUIContent("I have a tooltip", "The button overrides the box"));
+	GUI.Label(Rect(10,40,100,100), GUI.tooltip);
+
+	windowRect = GUI.Window(0, windowRect, DoMyWindow, "My Window");
+
+    GUI.color=Color.red;
+    windowRect0=GUI.Window(1, windowRect0, DoMyWindow2, "Red Window");
+    GUI.color=Color.green;
+    windowRect1=GUI.Window(2, windowRect1, DoMyWindow2, "Green Window");
+
+	if(!aTexture){
+        Debug.LogError("Assign a Texture in the inspector.");
+        return;
+    }
+    GUI.DrawTexture(Rect(260, 100, 150, 150), aTexture);
+
+	toggleTxt=GUI.Toggle(Rect(550, 180, 100, 30), toggleTxt, "A Toggle text");
+    toggleImg=GUI.Toggle(Rect(550, 210, 50, 50), toggleImg, aTexture);
+}
+
+function DoMyWindow(windowID:int) {
+	GUI.DragWindow(Rect(0,0,10000,50));
+}
+
+function DoMyWindow2(windowID:int){
+    if(GUI.Button(Rect(10, 20, 100, 20), "Hello World2"))
+    print("Got a click in window with color"+GUI.color);
+    GUI.DragWindow(Rect(0, 0, 10000, 10000));
+}
