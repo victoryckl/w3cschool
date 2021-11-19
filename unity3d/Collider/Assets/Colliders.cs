a494c5b653a03faaa6bd240fabdabc59505f113a
victoryckl <victoryckl2012@gmail.com>
2021-11-19 16:34:25 +0800

    添加unity3d/Collider 	new file:   unity3d/Collider/

---
+++ unity3d/Collider/Assets/Colliders.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colliders : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("OnTriggerEnter " + other.tag);
+        if (other.tag == "Pickup")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
