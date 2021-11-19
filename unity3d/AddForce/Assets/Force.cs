782e95a4d25d7ad4aa2a704478edb8da6924b4a2
victoryckl <victoryckl2012@gmail.com>
2021-11-19 16:35:27 +0800

    添加unity3d/AddForce

---
+++ unity3d/AddForce/Assets/Force.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Force : MonoBehaviour {
+
+    public GameObject addForceObject;
+
+	// Use this for initialization
+	void Start () {
+        addForceObject = GameObject.Find("Sphere");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnGUI()
+    {
+        if (GUILayout.Button("force", GUILayout.Height(50)))
+        {
+            addForceObject.GetComponent<Rigidbody>().AddForce(500, 0, 1000);
+        }
+    }
+}
