183d6fa3caad5ea823f1c9ca481d5c575eb917ad
victoryckl <victoryckl2012@gmail.com>
2021-11-19 16:30:01 +0800

    添加unity3d/Physics 	new file:   unity3d/Physics/

---
+++ unity3d/Physics/Assets/RigidbodyScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidbodyScript : MonoBehaviour {
+    public int speed = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDrag()
+    {
+        transform.position += Vector3.right * Time.deltaTime * Input.GetAxis("Mouse X") * speed;
+        transform.position += Vector3.up * Time.deltaTime * Input.GetAxis("Mouse Y") * speed;
+    }
+}
