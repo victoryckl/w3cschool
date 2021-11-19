d2b507ec87c2f3489e2e2d0fb58277c913b68e86
victoryckl <victoryckl2012@gmail.com>
2021-11-19 16:36:07 +0800

    添加unity3d/RayTest

---
+++ unity3d/RayTest/Assets/RayTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayTest : MonoBehaviour {
+    private int count = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton(0))
+        {
+            //从摄像机到单击处发出射线
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                //画出射线, 只有在Scene视图中才能看到
+                Debug.DrawLine(ray.origin, hitInfo.point);
+                GameObject gameObj = hitInfo.collider.gameObject;
+                //Debug.Log("click object name is " + gameObj.name);
+                //当射线碰撞目标的标签是Pickup时, 执行拾取操作
+                if (gameObj.tag == "Pickup")
+                {
+                    count++;
+                    Debug.Log("pick up! "+ count);
+                }
+            }
+        }
+	}
+}
