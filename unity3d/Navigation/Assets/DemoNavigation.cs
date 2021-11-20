62b3d8ac32502e009d8e4e6f2193cb7cf7313853
victoryckl <victoryckl2012@gmail.com>
2021-11-20 21:06:06 +0800

    添加 unity3d/Navigation/

---
+++ unity3d/Navigation/Assets/DemoNavigation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DemoNavigation : MonoBehaviour {
+    public Transform target;
+
+	// Use this for initialization
+	void Start () {
+        this.gameObject.GetComponent<NavMeshAgent>().destination = target.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
