53ef4d076855a677c9c1f77d9bed51f4dbcea784
victoryckl <victoryckl2012@gmail.com>
2021-11-24 23:06:51 +0800

    添加unity 	modified:   unity3d-bilibili/BaseScript/Assets/House.unity 	modified:   unity3d-bilibili/BaseScript/Assets/Scripts/Day04/VectorDemo.cs

--- unity3d-bilibili/BaseScript/Assets/Scripts/Day04/VectorDemo.cs
+++ unity3d-bilibili/BaseScript/Assets/Scripts/Day04/VectorDemo.cs
@@ -30,11 +30,19 @@ public class VectorDemo : MonoBehaviour {
     }
 
     //向量计算
-    public Transform t1, t2;
+    public Transform t1, t2, t3;
+    public float moveSpeed = 8;
     private void demo3()
     {
         Vector3 vec = t1.position - t2.position;
         // 指向被减向量(前面的），大小为两点间距，起点移动到坐标原点
         Debug.DrawLine(Vector3.zero, vec);
+
+        //t3沿着vec方向移动
+        if (Input.GetKey(KeyCode.A))
+        { //vec.normalized获取方向，避免两个物体间距对移动速度造成影响
+            t3.Translate(vec.normalized * Time.deltaTime * moveSpeed);
+            //同 t3.position += vec.normalized  * Time.deltaTime * moveSpeed;
+        }
     }
 }
