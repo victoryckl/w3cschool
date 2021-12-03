06c7098d1d7166e9a08b156176c0f1f614fc71a0
victoryckl <victoryckl2012@gmail.com>
2021-12-03 22:49:37 +0800

    添加unity 	new file:   unity3d-bilibili/BaseScene/Assets/Scripts.meta 	new file:   unity3d-bilibili/BaseScene/Assets/Scripts/testEventTrigger.cs 	new file:   unity3d-bilibili/BaseScene/Assets/Scripts/testEventTrigger.cs.meta 	new file:   unity3d-bilibili/BaseScene/Assets/UGUIScene.unity 	new file:   unity3d-bilibili/BaseScene/Assets/UGUIScene.unity.meta 	modified:   unity3d-bilibili/BaseScene/Assets/floor.jpg.meta 	new file:   unity3d-bilibili/BaseScene/Assets/hehua.png 	new file:   unity3d-bilibili/BaseScene/Assets/hehua.png.meta

---
+++ unity3d-bilibili/BaseScene/Assets/Scripts/testEventTrigger.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testEventTrigger : MonoBehaviour {
+
+	public void testTrigger()
+    {
+        Debug.Log("trigger");
+    }
+}
