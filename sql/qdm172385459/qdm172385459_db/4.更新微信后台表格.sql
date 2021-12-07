79ae7408d34818fdf35daf614031dae7dbb88ec5
victoryckl <victoryckl2012@gmail.com>
2021-12-07 15:39:05 +0800

    更新SQL 	modified:   "sql/qdm172385459/qdm172385459_db/3.\347\224\237\346\210\220\350\275\254\346\254\276\350\241\250\346\240\274.sql" 	modified:   "sql/qdm172385459/qdm172385459_db/4.\346\233\264\346\226\260\345\276\256\344\277\241\345\220\216\345\217\260\350\241\250\346\240\274.sql" 	modified:   "sql/qdm172385459/qdm172385459_db/5.\346\233\264\346\226\260\344\274\232\345\221\230ID.sql"

--- sql/qdm172385459/qdm172385459_db/4.更新微信后台表格.sql
+++ sql/qdm172385459/qdm172385459_db/4.更新微信后台表格.sql
@@ -1,4 +1,4 @@
 ##更新表(从EXCEL导入的表转换)
 ##更新微信后台表格
 insert into wp_tpxtransbwx(timestamp,timestr,wxorder,merchantID,money,remark,wxtoken,openid,commiter) 
-select unix_timestamp(cTime),cTime,wxorder,clientorder,money,remark,wxtoken,openid,status from wp_wechat_20211104_141458;
+select unix_timestamp(cTime),cTime,wxorder,clientorder,money,remark,wxtoken,openid,status from wp_wechat_20211207_153430;
