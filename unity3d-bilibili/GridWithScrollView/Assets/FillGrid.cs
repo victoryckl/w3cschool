f6a52e4203c57abd32c3150adfdc6b7d39434a18
victoryckl <victoryckl2012@gmail.com>
2021-12-14 13:53:08 +0800

    unity增加GridLayoutGroup和Prefab的实例 	new file:   unity3d-bilibili/GridWithScrollView/.gitignore 	new file:   unity3d-bilibili/GridWithScrollView/Assets/FillGrid.cs 	new file:   unity3d-bilibili/GridWithScrollView/Assets/FillGrid.cs.meta 	new file:   unity3d-bilibili/GridWithScrollView/Assets/Grid.unity 	new file:   unity3d-bilibili/GridWithScrollView/Assets/Grid.unity.meta 	new file:   unity3d-bilibili/GridWithScrollView/Assets/Image.prefab 	new file:   unity3d-bilibili/GridWithScrollView/Assets/Image.prefab.meta 	new file:   unity3d-bilibili/GridWithScrollView/Assets/OralItem.cs 	new file:   unity3d-bilibili/GridWithScrollView/Assets/OralItem.cs.meta 	new file:   unity3d-bilibili/GridWithScrollView/Assets/OralItem.prefab 	new file:   unity3d-bilibili/GridWithScrollView/Assets/OralItem.prefab.meta 	new file:   unity3d-bilibili/GridWithScrollView/Assets/PopulateGrid.cs 	new file:   unity3d-bilibili/GridWithScrollView/Assets/PopulateGrid.cs.meta 	new file:   unity3d-bilibili/GridWithScrollView/Assets/right.png 	new file:   unity3d-bilibili/GridWithScrollView/Assets/right.png.meta 	new file:   unity3d-bilibili/GridWithScrollView/ProjectSettings/AudioManager.asset 	new file:   unity3d-bilibili/GridWithScrollView/ProjectSettings/ClusterInputManager.asset 	new file:   unity3d-bilibili/GridWithScrollView/ProjectSettings/DynamicsManager.asset 	new file:   unity3d-bilibili/GridWithScrollView/ProjectSettings/EditorBuildSettings.asset 	new file:   unity3d-bilibili/GridWithScrollView/ProjectSettings/EditorSettings.asset 	new file:   unity3d-bilibili/GridWithScrollView/ProjectSettings/GraphicsSettings.asset 	new file:   unity3d-bilibili/GridWithScrollView/ProjectSettings/InputManager.asset 	new file:   unity3d-bilibili/GridWithScrollView/ProjectSettings/NavMeshAreas.asset 	new file:   unity3d-bilibili/GridWithScrollView/ProjectSettings/NetworkManager.asset 	new file:   unity3d-bilibili/GridWithScrollView/ProjectSettings/Physics2DSettings.asset 	new file:   unity3d-bilibili/GridWithScrollView/ProjectSettings/ProjectSettings.asset 	new file:   unity3d-bilibili/GridWithScrollView/ProjectSettings/ProjectVersion.txt 	new file:   unity3d-bilibili/GridWithScrollView/ProjectSettings/QualitySettings.asset 	new file:   unity3d-bilibili/GridWithScrollView/ProjectSettings/TagManager.asset 	new file:   unity3d-bilibili/GridWithScrollView/ProjectSettings/TimeManager.asset 	new file:   unity3d-bilibili/GridWithScrollView/ProjectSettings/UnityConnectSettings.asset

---
+++ unity3d-bilibili/GridWithScrollView/Assets/FillGrid.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FillGrid : MonoBehaviour {
+    public GameObject prefab;
+
+    public Transform parent;
+
+    public int numberToCreate;
+
+    // Use this for initialization
+    void Start () {
+        Fill();
+    }
+
+    void Fill()
+    {
+        GameObject newObj;
+
+        for (int i = 0; i < numberToCreate; i++)
+        {
+            newObj = Instantiate(prefab, parent);
+            newObj.GetComponent<Image>().color = Random.ColorHSV();
+        }
+    }
+}
---
+++ unity3d-bilibili/GridWithScrollView/Assets/OralItem.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OralItem : MonoBehaviour {
+
+    private static int number = 0;
+
+    private Text OralText;
+    private Image ResultImage;
+    private Image FlagImage;
+
+	// Use this for initialization
+	void Start () {
+        Transform textTf = transform.Find("Text");
+        OralText = textTf.GetComponent<Text>();
+
+        Transform resultTf = transform.Find("ImageBg/ImageResult");
+        ResultImage = resultTf.GetComponent<Image>();
+
+        Transform flagTf = transform.Find("ImageBg/ImageFlag");
+        FlagImage = resultTf.GetComponent<Image>();
+
+        OralText.color = Random.ColorHSV();
+        OralText.text = "哈哈  " + number+"   ";
+
+        ResultImage.color = Random.ColorHSV();
+
+        number++;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
